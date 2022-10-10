c15fab1aff587130b8cb1d5809d78b9f9bbd94fa
Pavel Anokhin <anohinpa@gmail.com>
2022-10-10 20:01:38 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Task 47
+
+Console.Clear();
+
+double[,] array = new double[new Random().Next(5, 10), new Random().Next(5, 10)];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Math.Round(new Random().NextDouble(), 2);
+        Console.Write($"{array[i, j]} ({i}{j})  ");
+    }
+    Console.WriteLine();
+}
