e96a1303c67763a7269d911666a46a3e8949419e
Pavel Anokhin <anohinpa@gmail.com>
2022-10-10 20:30:10 +0300

    Task50

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
-﻿// Task  47
+﻿// Task 50
 
 Console.Clear();
 
-double[,] array = new double[new Random().Next(5, 10), new Random().Next(5, 10)];
+int[,] array = new int[new Random().Next(5, 10), new Random().Next(5, 10)];
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = Math.Round(new Random().NextDouble(), 2);
-        Console.Write($"{array[i, j]} ({i}{j})  ");
+        array[i, j] = new Random().Next(3, 10);
+        Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
 }
 
+Console.WriteLine("Введите индексы элемента массива: ");
+int firstIndex = Convert.ToInt32(Console.ReadLine()); // first
+int secondIndex = Convert.ToInt32(Console.ReadLine()); // second
+
+if ((firstIndex < 0)||
+    (firstIndex >= array.GetLength(0))||
+    (secondIndex < 0)||
+    (secondIndex >= array.GetLength(1)))
+    Console.WriteLine("Такого элемента нет в массиве!");
+else
+    Console.WriteLine($"Значение элемента: {array[firstIndex, secondIndex]}");
